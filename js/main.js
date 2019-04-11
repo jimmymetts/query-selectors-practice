a9c97d94f95b490d22c6c0419a7542aa84b3afb8
Metts <metts@Mettss-MacBook-Pro.local>
2019-04-10 20:06:36 -0500

    practice

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Hello main.js");
+
+document.querySelector(".article__header").textContent = "Welcome to the Jimmy Metts Blog";
+
+document.querySelector(".article__header").classList.add("impotant")
+
+document.querySelector(".aside_box--dark").classList.remove("dashed")
+
+document.querySelector(".article__footer").classList.add("goldenrod")
